012f648de2b1a8bd991802ef6f285980c67d152e
Jeff Cox, KerussoTech <jeff@kerussotech.com>
2016-12-11 21:06:13 -0600

    ready to test

--- db_classes.py
+++ db_classes.py
@@ -4,15 +4,23 @@ from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
-class db_site(Base):
-	__tablename__ = 'site'
+class db_account(Base):
+	__tablename__ = 'account'
 	__table_args__ = {'schema': 'psh'}
 	id = sa.Column(sa.Integer, primary_key = True)
-	active = sa.Column(sa.Boolean)
-	url = sa.Column(sa.String)
-	user = sa.Column(sa.String)
-	password = sa.Column(sa.String)
+	network_id = sa.Column(sa.Integer)
+	site_id = sa.Column(sa.Integer)
+	auth = sa.Column(sa.JSON)
+	frequency = sa.Column(sa.Integer)
+	last = sa.Column(sa.Date)
 
+class db_network(Base):
+	__tablename__ = 'network'
+	__table_args__ = {'schema': 'psh'}
+	id = sa.Column(sa.Integer, primary_key = True)
+	name = sa.Column(sa.String)
+	auth = sa.Column(sa.JSON)
+	
 class db_post(Base):
 	__tablename__ = 'post'
 	__table_args__ = {'schema': 'psh'}
@@ -26,8 +34,16 @@ class db_post(Base):
 	slug = sa.Column(sa.String)
 	title = sa.Column(sa.String)
 	link = sa.Column(sa.String)
-	terms = sa.Column(sa.String)
-	sticky = sa.Column(sa.String)
 	app_create = sa.Column(sa.Date)
 	app_update = sa.Column(sa.Date)
+	fb_id = sa.Column(sa.String)
+	tw_id = sa.Column(sa.String)
 	
+class db_site(Base):
+	__tablename__ = 'site'
+	__table_args__ = {'schema': 'psh'}
+	id = sa.Column(sa.Integer, primary_key = True)
+	active = sa.Column(sa.Boolean)
+	url = sa.Column(sa.String)
+	user = sa.Column(sa.String)
+	password = sa.Column(sa.String)
--- social_push.py
+++ social_push.py
@@ -2,6 +2,8 @@ import datetime
 from wordpress_xmlrpc import Client
 from wordpress_xmlrpc.methods import posts
 from db_classes import *
+import twitter
+import bitly_api
 
 Session = sa.orm.sessionmaker()
 db_engine = sa.create_engine('postgresql+psycopg2://social_push:h3lpUte1l@localhost:5432/social_push?application_name=social_push')
@@ -9,25 +11,24 @@ db_connection = db_engine.connect()
 Session.configure(bind=db_engine)
 db_sqla = Session()
 
-#site = db_sqla.query(db_site).filter(db_site.active == True,db_site.id == 1).one()
 sites = db_sqla.query(db_site).filter(db_site.active == True)
 
 for site in sites:
 	client = Client(site.url, site.user, site.password)
-
+	
 	updates = client.call(posts.GetPosts({
-			'number': 100,
-			'offset': 0,
-			'orderby': 'post-modified', 
-			'order': 'DESC',
-			'post_type': 'post',
-			'post_status': 'publish'
-		}))
+		'number': 100,
+		'offset': 0,
+		'orderby': 'post-modified', 
+		'order': 'DESC',
+		'post_type': 'post',
+		'post_status': 'publish'
+	}))
 
 	now = datetime.datetime.now()
 	
 	for update in updates:
-	
+		
 		exists = db_sqla.query(db_post).filter(db_post.site_id == site.id,db_post.site_post_id == update.id).count()
 		if not exists:
 			db_sqla.add(db_post(site_id=site.id, site_post_id=update.id, modified_date=update.date_modified, publish_date=update.date, post_status=update.post_status, slug=update.slug, title=update.title, link=update.link, app_create=now, app_update=now))
@@ -35,4 +36,26 @@ for site in sites:
 			db_sqla.query(db_post).filter(db_post.site_id == site.id, db_post.site_post_id == update.id).\
 				update({db_post.modified_date: update.date_modified, db_post.publish_date: update.date, db_post.post_status: update.post_status, db_post.slug: update.slug, db_post.title: update.title, db_post.link: update.link, db_post.app_update: now})
 
+db_sqla.commit()
+
+access_token = 'd856875d28988706cd42b0347588a91273c70df7'
+bitly = bitly_api.Connection(access_token=access_token)
+
+for site in sites:
+	accounts = db_sqla.query(db_account).filter(db_account.site_id == site.id, is_active == 1)
+	if accounts:
+		for account in accounts:
+			now = datetime.datetime.now()
+			then = now - datetime.timedelta(0, 60 * (account.frequency - 2))
+			if then > account.last:
+				network = db_sqla.query(db_network).filter(db_network.id == account.network_id).one()
+				if network and network.name == 'Twitter':
+					twt = twitter.Api(consumer_key = network.auth->>'key', consumer_secret = network.auth->>'secret', access_token_key = account.auth->>'key', access_token_secret = account.auth->>'secret')
+					post = db_sqla.query(db_post).filter(db_post.site_id == site.id, db_post.tw_id == None, db_post.publish_date <= now ).order_by(db_post.publish_date.asc()).first()
+					short = bitly.shorten('http://google.com/')
+					text = post.title + ' ' + short['url']
+					status = twt.PostUpdate(text)
+					post.tw_id = status.id_str
+					account.last = now
+	
 db_sqla.commit()
