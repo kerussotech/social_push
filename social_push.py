b86f6be2a32e580c2dc794ba1afbcc0bdb12ccf9
Jeff Cox, KerussoTech <jeff@kerussotech.com>
2016-12-12 09:26:54 -0600

    more timezone cleanup

--- social_push.py
+++ social_push.py
@@ -4,6 +4,8 @@ from wordpress_xmlrpc.methods import posts
 from db_classes import *
 import twitter
 import bitly_api
+import calendar
+from datetime import datetime, timedelta
 
 Session = sa.orm.sessionmaker()
 db_engine = sa.create_engine('postgresql+psycopg2://social_push:h3lpUte1l@localhost:5432/social_push?application_name=social_push')
@@ -25,16 +27,24 @@ for site in sites:
 		'post_status': 'publish'
 	}))
 
-	now = datetime.datetime.now()
-	
+	now = datetime.now()
+		
 	for update in updates:
+		
+		utc_dt1 = update.date
+		timestamp1 = calendar.timegm(utc_dt1.timetuple())
+		local_dt1 = datetime.fromtimestamp(timestamp1)
+		
+		utc_dt2 = update.date_modified
+		timestamp2 = calendar.timegm(utc_dt2.timetuple())
+		local_dt2 = datetime.fromtimestamp(timestamp2)
 		
 		exists = db_sqla.query(db_post).filter(db_post.site_id == site.id,db_post.site_post_id == update.id).count()
 		if not exists:
-			db_sqla.add(db_post(site_id=site.id, site_post_id=update.id, modified_date=update.date_modified, publish_date=update.date, post_status=update.post_status, slug=update.slug, title=update.title, link=update.link, app_create=now, app_update=now))
+			db_sqla.add(db_post(site_id=site.id, site_post_id=update.id, modified_date=local_dt2, publish_date=local_dt1, post_status=update.post_status, slug=update.slug, title=update.title, link=update.link, app_create=now, app_update=now))
 		else:
 			db_sqla.query(db_post).filter(db_post.site_id == site.id, db_post.site_post_id == update.id).\
-				update({db_post.modified_date: update.date_modified, db_post.publish_date: update.date, db_post.post_status: update.post_status, db_post.slug: update.slug, db_post.title: update.title, db_post.link: update.link, db_post.app_update: now})
+				update({db_post.modified_date: local_dt2, db_post.publish_date: local_dt1, db_post.post_status: update.post_status, db_post.slug: update.slug, db_post.title: update.title, db_post.link: update.link, db_post.app_update: now})
 
 db_sqla.commit()
 
@@ -45,17 +55,17 @@ for site in sites:
 	accounts = db_sqla.query(db_account).filter(db_account.site_id == site.id, db_account.is_active == 1)
 	if accounts:
 		for account in accounts:
-			now = datetime.datetime.now()
-			then = now - datetime.timedelta(0, 60 * (account.frequency - 2))
-			if then > account.last:
+			now = datetime.now()
+			then = now - timedelta(0, 60 * (account.frequency - 2))
+			if (account.last and then > account.last) or account.last == None:
 				network = db_sqla.query(db_network).filter(db_network.id == account.network_id).one()
 				if network and network.name == 'Twitter':
-					twt = twitter.Api(consumer_key = network.auth['key'].astext, consumer_secret = network.auth['secret'].astext, access_token_key = account.auth['key'].astext, access_token_secret = account.auth['secret'].astext)
-					post = db_sqla.query(db_post).filter(db_post.site_id == site.id, db_post.tw_id == None, db_post.publish_date <= now ).order_by(db_post.publish_date.asc()).first()
-					short = bitly.shorten(post.link)
-					text = post.title + ' ' + short['url']
+					twt = twitter.Api(consumer_key = network.auth['key'], consumer_secret = network.auth['secret'], access_token_key = account.auth['key'], access_token_secret = account.auth['secret'])
+					next_post = db_sqla.query(db_post).filter(db_post.site_id == site.id, db_post.tw_id == None, db_post.publish_date <= now ).order_by(db_post.publish_date.asc()).first()
+					short = bitly.shorten(next_post.link)
+					text = next_post.title + ' ' + short['url']
 					status = twt.PostUpdate(text)
-					post.tw_id = status.id_str
+					next_post.tw_id = status.id_str
 					account.last = now
 	
 db_sqla.commit()
